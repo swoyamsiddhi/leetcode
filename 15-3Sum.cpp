8f6a2f0691482787273abfed4e14e71a5036a138
Swoyam siddhi pattanayak <swoyamsiddhipattanayak@gmail.com>
2026-08-19 23:00:44 +0530

    [15] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 15-3Sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        set<vector<int>> st;
+        int n = arr.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        vector<int> temp = {arr[i], arr[j], arr[k]};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+                }
+            }
+        }
+
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+    }
+};
