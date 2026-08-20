c8d7be7ba71660e2c34ddbc25677027740cf8640
Swoyam siddhi pattanayak <swoyamsiddhipattanayak@gmail.com>
2026-08-20 15:46:52 +0530

    [15] [Time Beats: 70.83%] [Memory Beats: 73.26%] - LeetPush

--- 15-3Sum.cpp
+++ 15-3Sum.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& arr) {
-        set<vector<int>> st;
-        int n = arr.size();
+        int n=arr.size();
+        sort(arr.begin(), arr.end());
+        // Store final result
+        vector<vector<int>> ans;
 
+        // First loop for first element
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (arr[i] + arr[j] + arr[k] == 0) {
-                        vector<int> temp = {arr[i], arr[j], arr[k]};
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
-                    }
+            // Skip duplicates for first element
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+
+            // Two pointers
+            int left = i + 1, right = n - 1;
+
+            // Find pairs for current arr[i]
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if (sum == 0) {
+                    ans.push_back({arr[i], arr[left], arr[right]});
+                    left++, right--;
+
+                    // Skip duplicates for left
+                    while (left < right && arr[left] == arr[left - 1]) left++;
+                    // Skip duplicates for right
+                    while (left < right && arr[right] == arr[right + 1]) right--;
                 }
+                else if (sum < 0) left++;
+                else right--;
             }
         }
-
-        vector<vector<int>> ans(st.begin(), st.end());
         return ans;
     }
 };
