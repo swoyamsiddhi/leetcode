c55f858993c0a4157d92f4eb9620d0b5926687c7
Swoyam siddhi pattanayak <swoyamsiddhipattanayak@gmail.com>
2026-08-22 22:16:53 +0530

    [169] [Time Beats: 100.00%] [Memory Beats: 75.93%] - LeetPush

---
+++ 169-Majority-Element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int el;
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                el = nums[i];
+                cnt = 1;
+            }
+            else if (nums[i] == el) {
+                cnt++;
+            }
+            else {
+                cnt--;
+            }
+        }
+
+        return el;
+    }
+};
