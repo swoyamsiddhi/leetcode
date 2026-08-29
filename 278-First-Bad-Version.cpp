36b7496ab5b3fa0e9f2b400952fb560a49ed2737
Swoyam siddhi pattanayak <swoyamsiddhipattanayak@gmail.com>
2026-08-29 20:55:16 +0530

    [278] [Time Beats: 57.49%] [Memory Beats: 72.22%] - LeetPush

--- 278-First-Bad-Version.cpp
+++ 278-First-Bad-Version.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int low = 1;
-        int high = n;
+        int l = 1, r = n;
 
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-
-            if (isBadVersion(mid)) {
-                high = mid;
-            }
-            else {
-                low = mid + 1;
-            }
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (isBadVersion(m))
+                r = m;
+            else
+                l = m + 1;
         }
 
-        return low;
+        return l;
     }
 };
