cc75dff59deec89ed733a79965f099abac525a9f
Swoyam siddhi pattanayak <swoyamsiddhipattanayak@gmail.com>
2026-09-01 16:05:32 +0530

    [16] [Time Beats: 5.01%] [Memory Beats: 85.85%] - LeetPush

---
+++ 16-3Sum-Closest.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int n = nums.size();
+        int closest = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+
+                    int sum = nums[i] + nums[j] + nums[k];
+
+                    if (abs(sum - target) < abs(closest - target)) {
+                        closest = sum;
+                    }
+                }
+            }
+        }
+
+        return closest;
+    }
+};
