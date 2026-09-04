1c85762b93c08932302ae201646fb7e792837540
Swoyam siddhi pattanayak <swoyamsiddhipattanayak@gmail.com>
2026-09-04 17:21:46 +0530

    [283] [Time Beats: 100.00%] [Memory Beats: 5.50%] - LeetPush

--- 283-Move-Zeroes.cpp
+++ 283-Move-Zeroes.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                swap(nums[i], nums[j]);
-                j++;
+        int n=nums.size();
+        vector<int>temp;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+            temp.push_back(nums[i]);
             }
         }
+
+        int nz=temp.size();
+        for(int i=0;i<nz;i++){
+            nums[i]=temp[i];
+        }
+        for(int i=nz;i<n;i++){
+            nums[i]=0;
+        }
+
     }
 };
